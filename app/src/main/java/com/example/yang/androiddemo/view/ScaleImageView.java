7c3a7c3d0e2ee00b680e6a4bda495e4b2f24b4f4
yangjiajia-company <875880715@qq.com>
2016-07-24 23:49:52 +0800

    可以縮放了

--- app/src/main/java/com/example/yang/androiddemo/view/ScaleImageView.java
+++ app/src/main/java/com/example/yang/androiddemo/view/ScaleImageView.java
@@ -2,6 +2,7 @@ package com.example.yang.androiddemo.view;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,6 +15,12 @@ import android.widget.ImageView;
 public class ScaleImageView extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener {
     private static final String TAG = "ScaleImageView";
     private boolean mOnce = true;
+
+    private  float mInitScale;
+    private  float mMaxScale;
+    private  float mMidScale;
+
+    private Matrix mMatrix;
 
     private Context mContext;
 
@@ -29,6 +36,7 @@ public class ScaleImageView extends ImageView implements ViewTreeObserver.OnGlob
         super(context, attrs, defStyleAttr);
         mContext = context;
         setScaleType(ScaleType.MATRIX);
+        mMatrix = new Matrix();
         init(attrs, defStyleAttr);
     }
 
@@ -63,8 +71,35 @@ public class ScaleImageView extends ImageView implements ViewTreeObserver.OnGlob
             if (d==null) {
                 return;
             }
+            int dw = d.getIntrinsicWidth();
+            int dh = d.getIntrinsicHeight();
 
             float scale = 1.0f;
+
+            if (dw > width && dh < height) {
+                scale = width * 1.0f / dw;
+            }
+
+            if (dh > height && dw < width) {
+                scale = height * 1.0f / dh;
+            }
+
+            if ((dw > width && dh > height) || (dw < width && dh < height)) {
+                scale = Math.min(width * 1.0f / dw,height * 1.0f /dh);
+            }
+
+            Log.d(TAG, "onGlobalLayout: scale=" + scale);
+            mInitScale = scale;
+            mMidScale = scale * 2;
+            mMaxScale = scale * 4;
+
+            int dx = width / 2 - dw / 2;
+            int dy = height / 2 - dh / 2;
+
+            mMatrix.postTranslate(dx,dy);
+            mMatrix.postScale(mInitScale, mInitScale, width / 2, height / 2);
+
+            setImageMatrix(mMatrix);
 
             mOnce =false;
         }
